04b69d173bd601d0667d310e26901b0cfded458f
pgmman <code0gm@gmail.com>
2025-02-07 17:20:18 +0900

    기본테스트 구현. 버전문제로 나는 오류들 해결

---
+++ realtimetest.py
@@ -0,0 +1,39 @@
+import mss #mss 버전이 최신이 10인데 오류나서 8로 재설치함 pip install mss==8.0.0
+import numpy as np
+import cv2
+import time
+import keyboard
+import torch #cuda118로 설치함 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+import win32gui
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+def get_active_window_client_rect():
+    hwnd = win32gui.GetForegroundWindow()
+    client_rect = win32gui.GetClientRect(hwnd)
+    left, top = win32gui.ClientToScreen(hwnd, (client_rect[0], client_rect[1]))
+    right, bottom = win32gui.ClientToScreen(hwnd, (client_rect[2], client_rect[3]))
+    return left, top, right, bottom
+
+size = get_active_window_client_rect()
+print(size)
+with mss.mss() as sct:
+    monitor = {"top": 50, "left": 50, "width": 1600, "height": 1000}
+    
+while True:
+    t = time.time()
+    
+    img = np.array(sct.grab(monitor))
+    
+    result = model(img)
+    
+    cv2.imshow("result", np.squeeze(result.render()))
+    
+    print('fps:', 1/(time.time()-t))
+    
+    cv2.waitKey(1)
+    
+    if keyboard.is_pressed('q'):
+        break
+    
+cv2.destroyAllWindows()
---
+++ test.py
@@ -0,0 +1,23 @@
+import torch
+
+# CUDA 사용 가능 여부 확인
+print("CUDA 사용 가능:", torch.cuda.is_available())
+
+# YOLOv5 모델 로드
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+print("모델 로드 성공")
+
+# 이미지 경로 정의
+img = 'https://xn--7o0by2jf8di3kbmew0bjy1b.com/wp-content/gallery/1_natural_family/4.jpg'
+print("이미지 경로:", img)
+
+# 추론 수행
+results = model(img)
+print("추론 완료")
+
+# 결과 출력 및 표시
+results.print()
+results.show()
+
+# 예측 출력
+print("예측 결과:", results.xyxy[0])  # img1 예측 (tensor)
